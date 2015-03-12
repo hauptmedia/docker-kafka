bf0ebfb76ee6db5a1ca7a574c44adbee2e0e9d2a
Julian Haupt <julian.haupt@hauptmedia.de>
2015-03-12 19:51:04 +0100

    made HOST_NAME & ADVERTISED_HOST_NAME configurable via env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-IP=$(hostname --ip-address)
+if [ -z "$HOST_NAME" ]; then
+	#use container's ip address if host name was specified
+	HOST_NAME=$(hostname --ip-address)
+fi
+
+if [ -z "$ADVERTISED_HOST_NAME" ]; then
+	#use container's ip address if host name was specified
+	ADVERTISED_HOST_NAME=$(hostname --ip-address)
+fi
+
+sed 's/#advertised.host.name=<hostname routable by clients>/'advertised.host.name=${ADVERTISED_HOST_NAME}'/' -i config/server.properties
+sed 's/#host.name=localhost/'host.name=${HOST_NAME}'/' -i config/server.properties
 
 if [ -n "$ZOOKEEPER_CONNECT" ]; then
 	sed 's/zookeeper.connect=localhost:2181/'zookeeper.connect=${ZOOKEEPER_CONNECT}'/' -i config/server.properties
 fi
-
-sed 's/#advertised.host.name=<hostname routable by clients>/'advertised.host.name=$IP'/' -i config/server.properties
-sed 's/#host.name=localhost/'host.name=$IP'/' -i config/server.properties
 
 exec "$@"

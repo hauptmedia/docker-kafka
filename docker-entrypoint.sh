cdc415eea6583c2fff6716148911169e4878a131
Julian Haupt <julian.haupt@hauptmedia.de>
2015-03-12 19:05:37 +0100

    made ZOOKEEPER_CONNECT variable optional

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z "$ZOOKEEPER_NAME" ]; then
-	echo "Missing linked zookeeper container"
-	exit 1
-fi
-
 IP=$(hostname --ip-address)
 
-sed 's/zookeeper.connect=localhost:2181/'zookeeper.connect=${ZOOKEEPER_PORT_2181_TCP_ADDR}:${ZOOKEEPER_PORT_2181_TCP_PORT}'/' -i config/server.properties
+if [ -n "$ZOOKEEPER_CONNECT" ]; then
+	sed 's/zookeeper.connect=localhost:2181/'zookeeper.connect=${ZOOKEEPER_CONNECT}'/' -i config/server.properties
+fi
+
 sed 's/#advertised.host.name=<hostname routable by clients>/'advertised.host.name=$IP'/' -i config/server.properties
 sed 's/#host.name=localhost/'host.name=$IP'/' -i config/server.properties
 

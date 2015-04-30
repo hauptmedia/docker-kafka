e93e904e13cd35a8e10db145a81510c078006240
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-30 10:13:30 +0200

    added more variables

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ```bash
 docker run -d \
 -e BROKER_ID=1 \
+-e LOG_DIRS=/tmp/kafka-logs \
+-e ZOOKEEPER_CONNECT=zk1:2181,zk2:2181,zk3:2181 \
 hauptmedia/kafka
 ```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,13 +12,13 @@ if [ -z "$ADVERTISED_HOST_NAME" ]; then
 fi
 
 BROKER_ID=${BROKER_ID:-0}
+ZOOKEEPER_CONNECT=${ZOOKEEPER_CONNECT:-localhost:2181}
+LOG_DIRS=${LOG_DIRS:-/tmp/kafka-logs}
 
 sed 's/broker.id=0/broker.id=${BROKER_ID}/' -i config/server.properties
 sed 's/#advertised.host.name=<hostname routable by clients>/'advertised.host.name=${ADVERTISED_HOST_NAME}'/' -i config/server.properties
 sed 's/#host.name=localhost/'host.name=${HOST_NAME}'/' -i config/server.properties
-
-if [ -n "$ZOOKEEPER_CONNECT" ]; then
-	sed 's/zookeeper.connect=localhost:2181/'zookeeper.connect=${ZOOKEEPER_CONNECT}'/' -i config/server.properties
-fi
+sed 's/zookeeper.connect=localhost:2181/'zookeeper.connect=${ZOOKEEPER_CONNECT}'/' -i config/server.properties
+sed "s/log.dirs=\/tmp\/kafka-logs/log.dirs=${LOG_DIRS}/" -i config/server.properties
 
 exec "$@"
